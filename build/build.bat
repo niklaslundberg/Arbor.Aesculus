4b70c41ce19263295c02d105669cc2eda0ba7829
Niklas Lundberg <info@nlundberg.se>
2018-08-27 21:48:30 +0200

    Updated build script.

--- build/build.bat
+++ build/build.bat
@@ -1,18 +1,5 @@
-SET OldPath=%~dp0
-
 SET Arbor.X.MSBuild.NuGetRestore.Enabled=true
 
-CD ..
-
-dotnet restore
-
-
-REM CD src
-REM CD Arbor.Aesculus.Core
-REM dotnet restore
-
-CD "%OldPath%"
-
-CALL Build.exe
+CALL dotnet arbor-build
 
 EXIT /B %ERRORLEVEL%
