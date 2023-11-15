505ce263acf4fe47ddcb85bb8cdc6804b732242e
Niklas Lundberg <info@nlundberg.se>
2023-11-15 11:01:27 +0100

    Use file scoped namespaces.

--- tests/Arbor.Aesculus.Tests.Integration/when_calling_find_vcs_path_without_params_in_this_solution.cs
+++ tests/Arbor.Aesculus.Tests.Integration/when_calling_find_vcs_path_without_params_in_this_solution.cs
@@ -3,23 +3,22 @@ using Arbor.Aesculus.Core;
 using Machine.Specifications;
 using NCrunch.Framework;
 
-namespace Arbor.Aesculus.Tests.Integration
+namespace Arbor.Aesculus.Tests.Integration;
+
+[Subject(typeof(VcsPathHelper))]
+public class when_calling_find_vcs_path_without_params_in_this_solution
 {
-    [Subject(typeof(VcsPathHelper))]
-    public class when_calling_find_vcs_path_without_params_in_this_solution
+    static string? vcsRootPath;
+
+    private Because of = () =>
     {
-        static string? vcsRootPath;
+        vcsRootPath = NCrunchEnvironment.NCrunchIsResident()
+            ? VcsPathHelper.FindVcsRootPath(new FileInfo(NCrunchEnvironment.GetOriginalSolutionPath())
+                .Directory?.FullName)
+            : Path.GetTempPath();
+    };
 
-        private Because of = () =>
-        {
-            vcsRootPath = NCrunchEnvironment.NCrunchIsResident()
-                ? VcsPathHelper.FindVcsRootPath(new FileInfo(NCrunchEnvironment.GetOriginalSolutionPath())
-                    .Directory?.FullName)
-                : Path.GetTempPath();
-        };
+    It should_not_return_null = () => vcsRootPath.ShouldNotBeNull();
 
-        It should_not_return_null = () => vcsRootPath.ShouldNotBeNull();
-
-        It should_return_an_existing_directory = () => Directory.Exists(vcsRootPath).ShouldBeTrue();
-    }
+    It should_return_an_existing_directory = () => Directory.Exists(vcsRootPath).ShouldBeTrue();
 }
--- tests/Arbor.Aesculus.Tests.Integration/when_calling_try_find_vcs_path_in_a_non_vcs_directory.cs
+++ tests/Arbor.Aesculus.Tests.Integration/when_calling_try_find_vcs_path_in_a_non_vcs_directory.cs
@@ -2,15 +2,14 @@
 using Arbor.Aesculus.Core;
 using Machine.Specifications;
 
-namespace Arbor.Aesculus.Tests.Integration
+namespace Arbor.Aesculus.Tests.Integration;
+
+[Subject(typeof(VcsPathHelper))]
+public class when_calling_try_find_vcs_path_in_a_non_vcs_directory
 {
-    [Subject(typeof(VcsPathHelper))]
-    public class when_calling_try_find_vcs_path_in_a_non_vcs_directory
-    {
-        static string? vcsRootPath;
+    static string? vcsRootPath;
 
-        Because of = () => { vcsRootPath = VcsPathHelper.TryFindVcsRootPath(Path.GetTempPath()); };
+    Because of = () => { vcsRootPath = VcsPathHelper.TryFindVcsRootPath(Path.GetTempPath()); };
 
-        It should_return_null = () => vcsRootPath.ShouldBeNull();
-    }
+    It should_return_null = () => vcsRootPath.ShouldBeNull();
 }
